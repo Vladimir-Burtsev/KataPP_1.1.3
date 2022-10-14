da987f91bc9ab3e1bb92e1857e8a8ea9a265adc5
vladimir burtsev <amakusl2@gmail.com>
2022-10-14 17:48:22 +0300

    add hibernate dependency to pom, clean to method main & DaoJDBC, add @annotations to User class, created hibetnate.cfg.xml

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,29 +1,62 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class Util {
 
-    private static Connection con = null;
-    private static String url = "jdbc:mysql:// localhost:3306/mydbtest";
-    private static String user = "root";
-    private static String pass = "root";
 
-    public static Connection getConnection(){
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("JDBC driver error.");
+//    private static final String URL = "jdbc:mysql:// localhost:3306/mydbtest";
+//    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+//    private static final String USER = "root";
+//    private static final String PASS = "root";
+//    private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
+
+    public static Session getSession (){
+
+
+        Configuration configuration = new Configuration();
+        Configuration configure = configuration.configure();
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            System.out.println("ok");
+            return session;
         }
-        try {
-            con = DriverManager.getConnection(url, user, pass);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Connection Error.");
+    }
+
+    public static SessionFactory getSessionFactory() {
+        SessionFactory sessionFactory = null;
+        if (sessionFactory == null){
+
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql:// localhost:3306/mydbtest");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "root");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+            sessionFactory = new Configuration()
+                    .addProperties(settings)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+            Session session = sessionFactory.openSession().getSession();
+            session.beginTransaction();
+            session.getTransaction().commit();
         }
-        return con;
+        return sessionFactory;
     }
 }
